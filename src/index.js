3ffb4cf8340c88185f600876cb0ba76b118dfc5d
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2022-08-07 15:11:14 +0300

    done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = {};
+  bracketsConfig.forEach(element => {
+    config[element[0]] = element[1];
+  });
+
+  let stack = [];
+
+  for (var i = 0; i < str.length; i++) {
+    
+    if (str[i] in config){
+      if(!(str[i] == config[str[i]] && stack.length > 0 && stack[stack.length-1] == str[i])){
+        stack.push(str[i]);
+        continue;
+      }
+    }
+
+    for (let k in config) {
+      if (config[k] === str[i]) {
+          if(stack.length == 0 || stack.pop() != k) {
+            return false;
+          }
+      }
+    }
+  }
+  return stack.length == 0;
 }
+
